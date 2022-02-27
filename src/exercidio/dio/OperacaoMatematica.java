d2aa9ef28c22b689465be893c3b5a96e65cf61e2
Milrem <milrem@gmail.com>
2022-02-26 22:08:34 -0300

    Criando e implementando uma interface

---
+++ src/exercidio/dio/OperacaoMatematica.java
@@ -0,0 +1,16 @@
+package exercidio.dio;
+// modelagem da interface
+
+    public interface OperacaoMatematica {
+
+        void soma(double num1, double num2);
+
+        void subtracao(double num1, double num2);
+
+        void multiplicacao(double num1, double num2);
+
+        void divisao(double num1, double num2);
+    }
+
+
+}
